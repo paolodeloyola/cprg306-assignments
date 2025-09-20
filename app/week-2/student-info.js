de575e906de80f8101c21219edcead00a0adb1a0
paolodeloyola <paolomdeloyola@gmail.com>
2025-09-19 19:39:02 -0600

    assignmnet 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Paolo Manalastas</p>
+      <p>https://github.com/paolodeloyola</p>
+    </div>
+  );
+}
